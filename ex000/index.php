e3347353e1ed9180bd0a2ce5a0351aa43efd827f
Rogério Gastão <eltrondemais@gmail.com>
2024-01-26 19:15:51 +0100

    php added

---
+++ ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <h1>Titulo do Servidor</h1>
+
+    <?php 
+
+        $nome = "Rogerio Gastao";
+
+        echo "bem-vindo $nome";
+        phpinfo();
+        
+    ?>
+    <?="nome de alguem" ?>
+</body>
+</html>
